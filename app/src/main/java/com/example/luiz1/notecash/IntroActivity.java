64daf8af9c274d63b7349ba493c6b68c00b5da95
heyimleaf <luiz1223@hotmail.com>
2018-03-03 22:01:46 -0300

    Animação de Introdução

---
+++ app/src/main/java/com/example/luiz1/notecash/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.example.luiz1.notecash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+    private static int SPLASH_TIMEOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(IntroActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+
+    }
+}
